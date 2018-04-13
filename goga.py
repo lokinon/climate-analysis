0c2e92b1f50cdfd90cb601bf1d5401ac7ea13cc1
Georgios Salavasidis <geosal@noc.ac.uk>
2018-04-13 15:32:32 +0100

    I dont care

--- goga.py
+++ goga.py
@@ -9,4 +9,4 @@ def inches_to_mm(inches):
     mm = inches * 25.4
     return mm
 
-# TODO(Georgios) Add here a line with a comment!!!
+# TODO(Georgios) Add here a line with a comment!!!you are not my boss to tell me what to do
